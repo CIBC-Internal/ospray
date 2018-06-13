869b9d8dc1b75772bf60b7be66f0575088f1921f
Dan White <dwhite@sci.utah.edu>
2018-06-13 15:28:24 -0600

    Try installing things

---
+++ ci/linux_install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y install wget cmake g++ clang git libtbb-dev libopenmpi-dev libgl1-mesa-dev libxrandr-dev libxinerama-dev libxcursor-dev alien
+
