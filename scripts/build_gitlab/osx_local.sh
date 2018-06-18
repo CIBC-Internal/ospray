04076516cd4f696dfdd6251c4e384b7ab6e65f74
Dan White <dwhite@sci.utah.edu>
2018-06-18 16:05:18 -0600

    Update script

--- scripts/build_gitlab/osx_local.sh
+++ scripts/build_gitlab/osx_local.sh
@@ -23,16 +23,16 @@ wget https://github.com/embree/embree/releases/download/v3.2.0/embree-3.2.0.x86_
 tar -xvf embree-3.2.0.x86_64.macosx.tar.gz
 
 cd ../..
-wget https://downloads.sourceforge.net/project/ispcmirror/v1.9.2/ispc-v1.9.2-osx.tar.gz && tar -xvf ispc-v1.9.2-osx.tar.gz && rm ispc-v1.9.2-osx.tar.gz
+wget https://downloads.sourceforge.net/project/ispcmirror/v1.9.2/ispc-v1.9.2-osx.tar.gz --no-check-certificate && tar -xvf ispc-v1.9.2-osx.tar.gz && rm ispc-v1.9.2-osx.tar.gz
 cd ospray/bin_local
 
 wget https://github.com/01org/tbb/releases/download/2018_U4/tbb2018_20180411oss_mac.tgz
 tar -xvf tbb2018_20180411oss_mac.tgz
 
 cmake \
--D embree_DIR:PATH=build/embree-3.2.0.x86_64.macosx \
+-D embree_DIR:PATH=bin_local/embree-3.2.0.x86_64.macosx \
 -D OSPRAY_TASKING_SYSTEM=TBB \
--D TBB_ROOT:PATH=build/tbb2018_20180411oss \
+-D TBB_ROOT:PATH=bin_local/tbb2018_20180411oss \
 -D OSPRAY_ENABLE_TESTING=ON \
 -D OSPRAY_SG_CHOMBO=OFF \
 -D OSPRAY_SG_OPENIMAGEIO=OFF \
